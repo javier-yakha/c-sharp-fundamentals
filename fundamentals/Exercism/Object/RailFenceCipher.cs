3a9c0efb4f8f40e0fcccbccd3813697be80efd44
Javier <javier@yakha.eu>
2024-04-15 14:21:20 +0200

    feat: RailFenceCipher - Encode

---
+++ fundamentals/Exercism/Object/RailFenceCipher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals.Exercism.Object
+{
+    public class RailFenceCipher
+    {
+        int rails;
+        public RailFenceCipher(int rails)
+        {
+            this.rails = rails;
+        }
+
+        public string Encode(string input)
+        {
+            string output = "";
+            string[] levels = new string[rails];
+
+            int counter = 0;
+            bool downwards = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                levels[counter] += input[i];
+
+                if (downwards && counter + 1 >= rails)
+                {
+                    counter--;
+                    downwards = !downwards;
+                }
+                else if (downwards) counter++;
+
+                else if (counter -1 < 0)
+                {
+                    counter++;
+                    downwards = !downwards;
+                }
+                else counter--;
+            }
+            foreach (string level in levels)
+            {
+                output += level;
+            }
+            return output;
+            
+        }
+
+        public string Decode(string input)
+        {
+
+            return string.Empty;
+        }
+    }
+}
--- fundamentals/Program.cs
+++ fundamentals/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Fundamentals
+﻿using Fundamentals.Exercism.Object;
+namespace Fundamentals
 {
 
     class MyFirstProgram
@@ -6,6 +7,7 @@
 
         public static void Main(string[] args)
         {
+            /*
             Car BMW = new Car(1998);
             Console.WriteLine(BMW.color);
             BMW.printColor();
@@ -13,6 +15,22 @@
 
             Car audi = new Car(1994);
             Console.WriteLine(audi.color);
+            */
+            RailFenceCipher cipher2 = new(2);
+            Console.WriteLine(cipher2.Encode("XOXOXOXOXO"));
+
+            RailFenceCipher cipher3 = new(3);
+            Console.WriteLine(cipher3.Encode("WEAREDISCOVEREDFLEEATONCE"));
+            var msg3 = "TEITELHDVLSNHDTISEIIEA";
+
+            RailFenceCipher cipher4 = new(4);
+            Console.WriteLine(cipher4.Encode("EXERCISES"));
+
+            RailFenceCipher cipher5 = new(5);
+            var msg5 = "EIEXMSMESAORIWSCE";
+
+            RailFenceCipher cipher6 = new(6);
+            var msg6 = "133714114238148966225439541018335470986172518171757571896261";
 
         }
     }
