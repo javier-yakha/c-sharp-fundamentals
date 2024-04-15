c66229e24f030726b6ebcede513631f0e35af924
Javier <javier@yakha.eu>
2024-04-15 12:42:55 +0200

    feat: Exercism - SpaceAge

---
+++ fundamentals/Exercism/Object/SpaceAge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals.Exercism.Object
+{
+    public class SpaceAge
+    {
+        private double years;
+        public SpaceAge(int seconds)
+        {
+            double minutes = (double)seconds / 60.0d;
+            double hours = minutes / 60.0d;
+            double days = hours / 24.0d;
+            years = days / 365.25d;
+        }
+        public double OnEarth() => years / 1.0d;
+
+        public double OnMercury() => years / 0.2408467d;
+
+        public double OnVenus() => years / 0.61519726d;
+
+        public double OnMars() => years / 1.8808158d;
+
+        public double OnJupiter() => years / 11.862615d;
+
+        public double OnSaturn() => years / 29.447498d;
+
+        public double OnUranus() => years / 84.016846d;
+
+        public double OnNeptune() => years / 164.79132d;
+    }
+}
--- fundamentals/Yakha-modules/Car.cs
+++ fundamentals/Yakha-modules/Car.cs
@@ -10,12 +10,22 @@ namespace Fundamentals
     {
         public string color = "Red"; // Class members
         public int yearOfMaking; // declaring a variable
+        public static string colorStatic = "Yellow";
 
-        public Car(int year)
+        public Car ()
         {
-            yearOfMaking = year; // defining a variable
+            Console.WriteLine(colorStatic);
+        }
+        public Car (int yearOfMaking)
+        {
+            this.yearOfMaking = yearOfMaking; // defining a variable
             Console.WriteLine(yearOfMaking);
         }
+        public Car (string color)
+        {
+            this.color = color;
+        }
+
         public void printColor()
         {
             Console.WriteLine(color);
@@ -24,7 +34,5 @@ namespace Fundamentals
         {
             Console.WriteLine(color);
         }
-        
-
     }
 }
