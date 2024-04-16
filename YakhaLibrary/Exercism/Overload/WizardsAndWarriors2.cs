4ab87c2d15ec2410205b5d245e8eef6df6ccfcee
Javier <javier@yakha.eu>
2024-04-16 15:08:30 +0200

    add: WizardsAndWarriors2 -Exercism

---
+++ YakhaLibrary/Exercism/Overload/WizardsAndWarriors2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YakhaLibrary.Exercism.Overload
+{
+    static class GameMaster
+    {
+        public static string Describe(Character character) => $"You're a level {character.Level} {character.Class} with {character.HitPoints} hit points.";
+        public static string Describe(Destination destination) => $"You've arrived at {destination.Name}, which has {destination.Inhabitants} inhabitants.";
+        public static string Describe(TravelMethod travelMethod)
+        {
+            string msg = "You're traveling to your destination ";
+            if (travelMethod == TravelMethod.Walking) msg += "by walking.";
+            if (travelMethod == TravelMethod.Horseback) msg += "on horseback.";
+            return msg;
+        }
+        public static string Describe(Character character, Destination destination, TravelMethod travelMethod) => $"{GameMaster.Describe(character)} {GameMaster.Describe(travelMethod)} {GameMaster.Describe(destination)}";
+        public static string Describe(Character character, Destination destination) => $"{GameMaster.Describe(character)} You're traveling to your destination by walking. {GameMaster.Describe(destination)}";
+    }
+    class Character
+    {
+        public string Class { get; set; }
+        public int Level { get; set; }
+        public int HitPoints { get; set; }
+    }
+    class Destination
+    {
+        public string Name { get; set; }
+        public int Inhabitants { get; set; }
+    }
+    enum TravelMethod
+    {
+        Walking,
+        Horseback
+    }
+
+}
