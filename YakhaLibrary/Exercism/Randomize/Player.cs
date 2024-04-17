5dd12956f243b87e4afc36a784c625368a6dc6b0
Javier <javier@yakha.eu>
2024-04-17 14:14:35 +0200

    fix: changed Random to Randomize because of type collision

--- YakhaLibrary/Exercism/Randomize/Player.cs
+++ YakhaLibrary/Exercism/Randomize/Player.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace YakhaLibrary.Exercism.Random
+namespace YakhaLibrary.Exercism.Randomize
 {
     public static class Player
     {
