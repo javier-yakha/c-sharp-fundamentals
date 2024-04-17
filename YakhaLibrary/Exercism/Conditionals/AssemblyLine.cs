2fecc67534ff6b9b8e56d640b50c1066f77a52a4
Javier <javier@yakha.eu>
2024-04-17 09:30:31 +0200

    add : Leap Exercism

--- YakhaLibrary/Exercism/Conditionals/AssemblyLine.cs
+++ YakhaLibrary/Exercism/Conditionals/AssemblyLine.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace YakhaLibrary.Exercism.Basic
+namespace YakhaLibrary.Exercism.Conditionals
 {
     static class AssemblyLine
     {
---
+++ YakhaLibrary/Exercism/Conditionals/Leap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YakhaLibrary.Exercism.Conditionals
+{
+    public static class Leap
+    {
+        public static bool IsLeapYear(int year) => !(year % 4 != 0 ^ (year % 100 == 0 && year % 400 != 0));
+    }
+}
