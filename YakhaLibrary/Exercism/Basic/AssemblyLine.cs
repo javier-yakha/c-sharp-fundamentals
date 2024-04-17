4e63568023ccd7a79959ed9ae90f108addd2d312
Javier <javier@yakha.eu>
2024-04-17 08:57:16 +0200

    fix: switch over dict

--- YakhaLibrary/Exercism/Basic/AssemblyLine.cs
+++ YakhaLibrary/Exercism/Basic/AssemblyLine.cs
@@ -11,21 +11,15 @@ namespace YakhaLibrary.Exercism.Basic
         public static double SuccessRate(int speed)
         {
             // Dict for scalability <-> could also be an array
-            Dictionary<int, double> dict = new()
+            return speed switch
             {
-                { 0, 0 },
-                { 1, 1 },
-                { 2, 1 },
-                { 3, 1 },
-                { 4, 1 },
-                { 5, 0.9 },
-                { 6, 0.9 },
-                { 7, 0.9 },
-                { 8, 0.9 },
-                { 9, 0.8 },
-                { 10, 0.77 }
-            };
-            return dict[speed];
+                0 => 0,
+                1 or 2 or 3 or 4 => 1,
+                5 or 6 or 7 or 8 => 0.9,
+                9 => 0.8,
+                10 => 0.77,
+                _ => throw new ArgumentOutOfRangeException()
+            }; ;
         }
         public static double ProductionRatePerHour(int speed) => SuccessRate(speed) * (speed * 221);
         public static int WorkingItemsPerMinute(int speed) => (int)ProductionRatePerHour(speed) / 60;
