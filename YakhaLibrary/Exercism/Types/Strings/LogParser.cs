e94b41721c9cf49c3aa9bbf47f2b981fc1257f1d
Javier <javier@yakha.eu>
2024-04-24 11:24:18 +0200

    add: ParsingLogFiles - Exercism - WIP

--- YakhaLibrary/Exercism/Types/Strings/LogParser.cs
+++ YakhaLibrary/Exercism/Types/Strings/LogParser.cs
@@ -9,35 +9,14 @@ namespace YakhaLibrary.Exercism.Types.Strings
 {
     public class LogParser
     {
-        static string isValidLineRegex = "^[[](TRC|DBG|INF|WRN|ERR|FTL)[]]";
-        static string splitLogLineRegex = "<[=-*^]+>";
-        static string countQuotedPasswordsRegex = """password""";
-        static string removeEndOfLineTextRegex = "end-of-line[0-9]+";
-
-        public bool IsValidLine(string text)
-            {
-                Regex regex = new(isValidLineRegex);
-                return regex.IsMatch(text);
-            }
-
-        public string[] SplitLogLine(string text)
-        {
-            Regex regex = new(splitLogLineRegex);
-            return regex.Split(text);
-        }
-
-        public int CountQuotedPasswords(string lines)
-        {
-            Regex regex = new(countQuotedPasswordsRegex);
-            return regex.Count(lines);
-        }
-
-        public string RemoveEndOfLineText(string line)
-        {
-            Regex regex = new(removeEndOfLineTextRegex);
-            return regex.Replace(line, "");
-        }
-
+        static Regex isValidRE = new Regex("^[[](TRC|DBG|INF|WRN|ERR|FTL)[]]");
+        static Regex splitLogLineRegex = new Regex("<[-=*^]+>", RegexOptions.IgnoreCase);
+        static Regex countQuotedPasswordsRegex = new Regex("\".*password.*\"", RegexOptions.IgnoreCase);
+        static Regex removeEndOfLineTextRegex = new Regex("end-of-line[0-9]+");
+        public bool IsValidLine(string text) => isValidRE.IsMatch(text);
+        public string[] SplitLogLine(string text) => splitLogLineRegex.Split(text);
+        public int CountQuotedPasswords(string lines) => countQuotedPasswordsRegex.Count(lines);
+        public string RemoveEndOfLineText(string line) => removeEndOfLineTextRegex.Replace(line, "");
         public string[] ListLinesWithPasswords(string[] lines)
         {
             throw new NotImplementedException($"Please implement the LogParser.ListLinesWithPasswords() method");
